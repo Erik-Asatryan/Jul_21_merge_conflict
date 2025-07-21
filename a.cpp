d7a3a4a914dc9e600c105b448e02721513700cb7
AsErik01 <erik.asatryan1101@gmail.com>
2025-07-21 17:19:25 +0400

    Commit 5

--- a.cpp
+++ a.cpp
@@ -9,5 +9,6 @@ int main()
     std::cout << "Second commit" << std::endl;
     std::cout << "Third commit" << std::endl;
     std::cout << "4th commit" << std::endl;
+    std::cout << "5th commit" << std::endl;
     return 0;
 }

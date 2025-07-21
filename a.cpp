ce73268f807de37a215e3375138537094957baef
AsErik01 <erik.asatryan1101@gmail.com>
2025-07-21 17:19:06 +0400

    Commit 4

--- a.cpp
+++ a.cpp
@@ -8,5 +8,6 @@ int main()
     std::cout << "First commit" << std::endl;
     std::cout << "Second commit" << std::endl;
     std::cout << "Third commit" << std::endl;
+    std::cout << "4th commit" << std::endl;
     return 0;
 }
